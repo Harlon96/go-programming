957933f422880225586f9ffefb5554acdf5f0d8d
Harlon Hutchison <65577414+Harlon96@users.noreply.github.com>
2020-08-17 13:46:52 -0400

    `defer panic`

---
+++ deferpanic.go
@@ -0,0 +1,33 @@
+package main
+
+import(
+	"fmt"
+)
+
+// defer panic recover --
+
+func main() {
+     f()
+     fmt.Println("returned normally from f.")
+}
+
+func f() {
+	defer func() {
+		if r := recover(); r != nil {
+			   fmt.Println("recoverd in f",r)
+		}
+	}()
+	fmt.Println("calling g...")
+	g(0)
+	fmt.Println(`returned normally from g.`)
+}
+
+func g(i int) {
+	if i < 3 {
+		fmt.Println(`panicking!`)
+		panic(fmt.Sprintf("%v", i))
+	}
+	defer fmt.Println("defer in g", i)
+	fmt.Println("printing in g", i)
+	g(i + 1)
+}
